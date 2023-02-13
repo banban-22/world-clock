59206e0a9434e013286b27d7eee254162ac559fe
AyakaB <ayaka.bando22@gmail.com>
2023-02-13 08:50:34 -0800

    added basic component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="clock-container">
+      {/* Clock 1 */}
+      <form action="">
+        <input type="date" id="date1" />
+        <input type="time" id="time1" />
+        <div className="clock"></div>
+      </form>
+
+      {/* Clock2 */}
+      <form action="">
+        <input type="date" id="date1" />
+        <input type="time" id="time1" />
+        <div className="clock"></div>
+      </form>
     </div>
   );
 }
